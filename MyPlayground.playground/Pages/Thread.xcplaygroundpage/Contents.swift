50a4a1339fd82dd0c96095995941cab6d8615dee
Ahsan <ahsanul.kabir@bjitgroup.com>
2023-09-11 17:04:39 +0600

    thread

--- MyPlayground.playground/Pages/Thread.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Thread.xcplaygroundpage/Contents.swift
@@ -2,22 +2,38 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+//
+//print(Thread.isMainThread)
+//
+//
+//class ThreadTest{
+//    func createThread(){
+//        Thread.detachNewThreadSelector(#selector(printx), toTarget: self, with: nil)
+//    }
+//
+//    @objc func printx(){
+//        print("Thread loop running")
+//        print(Thread.isMainThread)
+//    }
+//}
 
-print(Thread.isMainThread)
+//
+//let threadClass = ThreadTest()
+//
+//threadClass.createThread()
+//
+//
+//let current = Thread.current
+//print("current thread",current,current.stackSize)
+//
+//
+//let t = Thread()
+//t.name = "secondary"
+//print("current thread",t,t.stackSize)
+//
+//let t1 = Thread()
+//t1.name = "secondary1"
+//print("secondary thread",t1,t1.stackSize)
+//
 
 
-class ThreadTest{
-    func createThread(){
-        Thread.detachNewThreadSelector(#selector(printx), toTarget: self, with: nil)
-    }
-    
-    @objc func printx(){
-        print("Thread loop running")
-    }
-}
-
-
-let threadClass = ThreadTest()
-
-threadClass.createThread()
